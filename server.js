ee35e414952e68a691ff34c8d8319b6dd08341a8
ThanachatAim <Thanachat.aim@gmail.com>
2023-02-14 19:20:15 +0700

    add Authentication

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
+const connectDB = require('./config/db');
+
+const hospitals = require('./routes/hospitals');
+const auth = require('./routes/auth');
+
+//Load env vars
+dotenv.config({path:'./config/config.env'});
+
+//Connect to database
+connectDB();
+
+const app=express();
+
+//Body parser
+app.use(express.json());
+
+//Cookie parser
+app.use(cookieParser());
+
+app.get('/', (req,res) => {
+    //1.  res.send('<h1>Hello from express</h1>');
+    //2.  res.send({name:'Brad'});
+    //3.  res.json({name:'Brad'});
+    //4.  res.sendStatus(400);
+    //5.  res.status(400).json({success:false});
+    res.status(200).json({success:true, data:{id:1}});
+});
+
+app.use('/api/v1/hospitals',hospitals);
+app.use('/api/v1/auth',auth);
+
+const PORT = process.env.PORT || 5000;
+const server = app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV,' mode on port ', PORT));
+
+//Handle unhandled promise rejections
+process.on('unhandledRejection',(err,promise)=>{
+    console.log(`Error: ${err.message}`);
+    //Close server & exit process
+    server.close(()=>process.exit(1));
+});
